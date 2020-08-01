34edf739be5ab5212b28e1518f19acf263768efc
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2020-08-02 01:34:05 +0530

    Create files2.py

---
+++ files2.py
@@ -0,0 +1,19 @@
+/* Write a program that prompts for a file name, then opens that file and reads through the file, looking for lines of the form:
+X-DSPAM-Confidence:    0.8475
+Count these lines and extract the floating point values from each of the lines and compute the average of those values. Do not use the sum() function or a variable named sum in your solution. */
+
+
+
+fname = input("Enter file name: ")
+fh = open(fname)
+count = 0
+total = 0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:") : continue
+    t=line.find("0")
+    number= float(line[t:])
+    count = count + 1
+    total = total + number
+
+average = total/count
+print("Average spam confidence:",average)
