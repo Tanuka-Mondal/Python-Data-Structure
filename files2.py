7979f2c79725a22532e1659fea607460f3721b86
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2020-08-02 22:41:25 +0530

    Update files2.py

--- files2.py
+++ files2.py
@@ -1,6 +1,8 @@
-/* Write a program that prompts for a file name, then opens that file and reads through the file, looking for lines of the form:
+"""
+Write a program that prompts for a file name, then opens that file and reads through the file, looking for lines of the form:
 X-DSPAM-Confidence:    0.8475
-Count these lines and extract the floating point values from each of the lines and compute the average of those values. Do not use the sum() function or a variable named sum in your solution. */
+Count these lines and extract the floating point values from each of the lines and compute the average of those values. Do not use the sum() function or a variable named sum in your solution. 
+"""
 
 
 
